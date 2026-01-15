39df46c910926765a1429416d276bbfc8482e4ff
leen alkhadraa <leenalkhadraa@gmail.com>
2026-01-15 19:45:20 +0100

    Login- und Registrierungslogik implementiert und mit dem Backend verbunden.

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule,RouterModule],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+    email = '';       // <-- hier
+  password = '';
+  error = '';
+
+ constructor( private auth: AuthService, private router: Router  ) {}
+ login() {
+   this.auth.login(this.email, this.password).subscribe({
+     next: () => {
+       this.error = '';
+       alert('Login erfolgreich!');
+ this.router.navigate(['/dashboard']);     },
+     error: err => {
+       this.error = err.error?.message || 'Login failed';
+     }
+   });
+ }
+
+}
